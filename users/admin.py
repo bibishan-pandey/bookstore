1e32235965084cc281c8c1003b3122a8e4443013
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2021-02-11 14:26:18 +0545

    feat: added custom user creation and change form along with some test cases

--- users/admin.py
+++ users/admin.py
@@ -1,42 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from users.forms import CustomUserChangeForm
+from users.forms import CustomUserCreationForm
+
+CustomUser = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
-    list_display = (
-        'email', 'username', 'first_name',
-        'last_name', 'is_staff', 'is_active',
-    )
-    list_filter = (
-        'email', 'username', 'first_name',
-        'last_name', 'is_staff', 'is_active',
-    )
-    fieldsets = (
-        (
-            None, {
-                'fields': (
-                    'email', 'password',
-                    'username', 'first_name', 'last_name',
-                ),
-            },
-        ),
-        ('Permissions', {'fields': ('is_staff', 'is_active')}),
-    )
-    add_fieldsets = (
-        (
-            None, {
-                'classes': ('wide',),
-                'fields': (
-                    'email', 'username', 'first_name', 'last_name',
-                    'password1', 'password2', 'is_staff', 'is_active',
-                ),
-            },
-        ),
-    )
-    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_display = ('email', 'username')
+    list_filter = ('email', 'username')
+    search_fields = ('email', 'username')
     ordering = ('email',)
 
 
--- users/tests.py
+++ users/tests.py
@@ -1,14 +1,32 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from rest_framework.test import APIClient
 
 
-class TestAPI(TestCase):
+class CustomUserTests(TestCase):
 
     def setUp(self):
-        user_obj = get_user_model().objects.create(email='testuser@gmail.com')
-        self.client = APIClient()
-        self.client.force_authenticate(user=user_obj)
+        self.User = get_user_model()
 
-    def test_method(self):
-        self.assertEqual('Test Demo', 'Test Demo')
+    def test_create_user(self):
+        user = self.User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='testPass123',
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        admin_user = self.User.objects.create_superuser(
+            username='super_admin',
+            email='superadmin@email.com',
+            password='testPass123',
+        )
+        self.assertEqual(admin_user.username, 'super_admin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
